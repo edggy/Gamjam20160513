892ab60f13b365fd611b22c0b366c02c2e1a558a
edggy <edggy@Ethan-Alienware>
2016-05-14 00:16:11 -0400

    Basic working version of the casino game

---
+++ client.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import time
+import socket
+import threading
+
+import curses
+
+class Player:
+	def __init__(self):
+		try:
+			server_info = (sys.argv[1], int(sys.argv[2]))
+		except:
+			# How did you mess this up?
+			print "Usage:\n%s [ip] [port]" % sys.argv[0]
+			raw_input("Press the any key to finish.")
+			sys.exit(1)
+
+		# Check if ip is in ipv4 format or ipv6 format
+		ipv4 = False
+		if '.' in server_info[0]: #this works, screw regex
+			ipv4 = True
+
+		# Connect to server
+		if ipv4:
+			self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		else:
+			self.server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+
+		# Connect to the server
+		connected = False
+
+		# Attempt to connect 10 times. If it fails all 10, give up.
+		for i in range(1, 11):
+			try:
+				self.server.connect(server_info)
+				connected = True
+				break
+			except:
+				print "Could not connect to the server (attempt #%d). Trying again." % i
+
+		if not connected:
+			print "Could not connect to the server, it might be down or super busy."
+			raw_input("Press the any key to finish.")
+			sys.exit(1)
+
+		self.tickets = -1
+		self.buckets = {}
+		self.numBuckets = 0
+		self.time = -1
+		self.myBuckets = {}
+		self.select = 0
+
+		self.alive = threading.Event()
+		self.change = False
+		self.data = ''
+
+	def recv(self):
+		self.data += self.server.recv(1024)
+		data, self.data = self.data.split('\0', 1)
+		return data
+	
+	def send(self, data):
+		self.server.send(str(data) + '\0')
+	
+	def __str__(self):
+		ret = ''
+		ret += 'Tickets:\t%s\t\tTime Left:\t%s\n' % (self.tickets, self.time)
+		for n in range(self.numBuckets):
+			ret += '%s\t' % n
+		for n in range(self.numBuckets):
+			if n in self.buckets:
+				ret += '%s\t' % self.buckets[n]
+			#else:
+				#self.server.send('bucket:%s' % n)
+				#ret += '??\t'
+		for n in range(self.numBuckets):
+			if n in self.myBuckets:
+				ret += '%s\t' % self.myBuckets[n]
+			else:
+				ret += '0\t'   
+		return ret
+
+	def doListen(self):
+		try:
+			while self.alive.isSet():
+				try:
+					
+					recv = self.recv().split(':')
+					#if recv[0] == 'req':
+						#self.server.send('req:%s' % raw_input(recv[1]))
+					#print recv
+					if recv[0] == 'tickets':
+						newTickets = float(recv[1])
+						if self.tickets != newTickets:
+							self.tickets = newTickets
+							self.change = True
+					elif recv[0] == 'buckets':
+						self.numBuckets = int(recv[1])                        
+					elif recv[0] == 'time':
+						self.time = float(recv[1])
+						self.change = True
+					elif recv[0] == 'bucket':
+						self.buckets[int(recv[1])] = int(recv[2])  
+						self.change = True
+					elif recv[0] == 'allbucket':  
+						self.numBuckets = int(recv[1])
+						for n in range(self.numBuckets):
+							self.buckets[n] = int(recv[n+2])
+						self.change = True
+					elif recv[0] == 'add':
+						try:
+							self.myBuckets[int(recv[1])] += 1
+							self.change = True
+						except:
+							pass
+				except socket.error:
+					self.send('error')
+		except socket.error:
+			pass
+		print 'ERROR'
+
+	def updateWindow(self, window):
+		maxY, maxX = window.getmaxyx()
+		window.erase()
+		window.addstr(0,0,'Tickets: %d' % self.tickets)
+		timeStr = 'Time Left: %d' % self.time
+		window.addstr(0, maxX - len(timeStr), timeStr)
+				
+		
+		if self.numBuckets > 0:
+			Yspace = maxY/8
+			Xspace = maxX/(self.numBuckets+1)
+			
+			window.addstr(Yspace,0,'Bin Number:')
+			window.addstr(Yspace*2,0, 'Ticket Count:')
+			window.addstr(Yspace*3,0, 'My Tickets:')			
+			
+			for n in range(self.numBuckets):
+				count = 0
+				mycount = 0
+				try:
+					count = self.buckets[n]
+					mycount = self.mybuckets[n]
+				except:
+					pass
+				
+				window.addstr(Yspace,(n+1)*Xspace + Xspace/2,str((n+1)))
+				window.addstr(Yspace*2,(n+1)*Xspace + Xspace/2, str(count))
+				window.addstr(Yspace*3,(n+1)*Xspace + Xspace/2, str(mycount))
+				if self.select == n:
+					window.addstr(Yspace*3+1,(n+1)*Xspace + Xspace/2, '^')
+					window.addstr(Yspace*3+2,(n+1)*Xspace + Xspace/2 - 1, 'add')
+		window.move(maxY-1, 0)
+		
+	def doAction(self, window = None):
+		self.send('tickets')
+		self.send('buckets')
+		if window is not None:
+			window.timeout(0)
+		self.lastCheck = 0
+		
+		while self.alive.isSet():
+			curTime = time.time() 
+			if curTime - self.lastCheck > 1:
+				self.send('time')
+				self.send('allbucket')
+				self.lastCheck = curTime
+			if window is None:
+				if self.change:
+					#os.system('cls')
+					print self
+					self.change = False
+			else:
+				self.updateWindow(window)
+				
+				c = window.getch()
+				if c == curses.ERR:
+					pass
+				elif c == curses.KEY_LEFT:
+					if self.select > 0:
+						self.select -= 1
+				elif c == curses.KEY_RIGHT:
+					if self.select < self.numBuckets - 1:
+						self.select += 1
+				elif c == ord(' '):
+					self.send('add:%s' % self.select)
+				elif c == ord('q'):
+					self.alive.clear()
+			
+
+	def run(self, window = None):
+		self.alive.set()
+		listenThread = threading.Thread(target=self.doListen ,args=())
+		listenThread.start()
+		actionThread = threading.Thread(target=self.doAction ,args=(window,))
+		actionThread.start()
+		listenThread.join()
+		actionThread.join()
+
+if __name__ == "__main__":
+	client = Player()
+	curses.wrapper(client.run)
+	#client.run()
---
+++ server.py
@@ -0,0 +1,267 @@
+import json
+import time
+import random
+import socket
+import threading
+import datetime
+
+from filelock import FileLock
+
+class Player:
+	def __init__(self, connection, client_address):
+		self.connection = connection
+		self.client_address = client_address
+		
+		self.tickets = 100.0
+		
+		#self.name = self.get('What is your name?')
+		self.name = '%s:%s' % self.client_address
+		
+		self.data = ''
+		
+	def depositTicket(self, bucket):
+		if self.tickets >= 1:
+			bucket += Ticket(self)
+			self.tickets -= 1
+			return True
+		return False
+	
+	def __hash__(self):
+		return hash(self.client_address)
+	
+	def get(self, info):
+		while True:
+			self.connection.sendall('req:%s' % info)
+			result = self.connection.recv(1024).split(':')
+			if result[0] == 'req':
+				return result[1]
+			
+	
+	def serve(self):
+		self.data += self.connection.recv(1024)
+		data, self.data = self.data.split('\0', 1)
+		return data
+	
+	def send(self, data):
+		self.connection.sendall(data)
+
+class Ticket:
+	def __init__(self, player):
+		self.owner = player
+		self.number = random.random()
+		
+	def __hash__(self):
+		return int(self.number * 2**32)
+
+class Bucket:
+	def __init__(self):
+		self.tickets = set([])
+		self.available = True
+	
+	def addTicket(self, ticket):
+		if not self.available or ticket in self.tickets:
+			return False
+		
+		self.tickets.add(ticket)
+		return True
+	
+	def __iadd__(self, ticket):
+		self.addTicket(ticket)
+	
+	def __len__(self):
+		return self.numTickets()
+	
+	def numTickets(self):
+		return len(self.tickets)
+	
+	def empty(self):
+		self.tickets.clear()
+
+class GameServer:
+	def __init__(self, settingFile):
+		with open(settingFile) as f:
+			self._settings = json.load(f)
+		self.alive = threading.Event()
+		self.threads = []
+		self.players = []
+		self.startTime = 0
+		self.now = time.time
+		self.roundNumber = 0
+		self.buckets = [Bucket() for n in range(self._settings['num_buckets'])]
+		self.house = 0
+		
+		
+	def startRound(self):
+		for b in self.buckets:
+			b.empty()
+			b.available = True
+		self.startTime = self.now()
+		self.roundNumber += 1
+	
+	def endRound(self):
+		for b in self.buckets:
+			b.available = False
+			
+		minBuckets = None
+		minVal = None
+		
+		for b in self.buckets:
+			tix = b.numTickets()
+			if minVal is None or tix < minVal:
+				minBuckets = [b]
+				minVal = tix
+			elif tix == minVal:
+				minBuckets.append(b)
+		
+		# The prize is equal to the number of buckets but is divided among all smallest buckets
+		prize = float(len(self.buckets)) / len(minBuckets)
+		
+		for b in minBuckets:
+			for t in b.tickets:
+				t.owner.tickets += prize
+				self.house -= prize
+				
+		self.log('House:\t%s tickets' % (self.house), 5)
+		for p in self.players:
+			try:
+				p.send('tickets:%s\0' % p.tickets)
+				self.log('%s:\t%s tickets' % (p.name, p.tickets), 5)
+			except socket.error:
+				pass
+		
+	
+	def doRound(self):
+		while self.alive.isSet():
+			self.startRound()
+			time.sleep(int(self._settings['round_time']))
+			self.endRound()
+			
+	def doPlayer(self, player):
+		try:
+			while self.alive.isSet():
+				try:
+					request = player.serve().split(':')
+					if request[0] == 'time':
+						player.send('time:%s\0' % (int(self._settings['round_time']) - self.now() + self.startTime))
+						self.log('%s:\trequested time' % player.name, 10)
+					elif request[0] == 'buckets':
+						player.send('buckets:%s\0' % len(self.buckets))
+						self.log('%s:\trequested buckets' % player.name, 10)
+					elif request[0] == 'tickets':
+						player.send('tickets:%s\0' % player.tickets)
+						self.log('%s:\trequested tickets:\t%s' % (player.name, player.tickets), 10)
+					elif request[0] == 'bucket':
+						num = int(request[1])
+						player.send('bucket:%s:%s\0' % (num, self.buckets[num].numTickets()))
+						self.log('%s:\trequested bucket:\t%s' % (player.name, num), 10)
+					elif request[0] == 'allbucket':
+						ret = 'allbucket:%s' % len(self.buckets)
+						for b in self.buckets:
+							ret += ':%s' % b.numTickets()
+						player.send(ret + '\0')
+						self.log('%s:\trequested all buckets' % player.name, 10)
+							
+					elif request[0] == 'add':
+						num = int(request[1])	
+						self.log('%s:\trequested add:\t%s' % (player.name, num), 10)
+						if self.buckets[num].addTicket(Ticket(player)):
+							player.tickets -= 1
+							self.house += 1
+							player.send('add:%s\0' % num)
+							player.send('tickets:%s\0' % player.tickets)
+							self.log('%s:\tnow has:\t%s tickets' % (player.name, player.tickets), 10)
+						else:
+							player.send('add:error\0')
+				except socket.error:
+					player.send('Error\0')
+				
+		except socket.error:
+			pass
+		
+		try:
+			player.connection.shutdown(SHUT_RDWR)
+		except:
+			pass
+		try:
+			player.connection.close()
+		except:
+			pass			
+	
+	def run(self):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_address = (self._settings['address'], int(self._settings['port']))
+		self.sock.bind(server_address)
+		self.sock.listen(5)
+
+		# Start looking for connections
+		# self._sock.listen(self._settings['listen_queue_length'])
+		
+		self.alive.set()
+		
+		roundThread = threading.Thread(target=self.doRound ,args=())
+		roundThread.start()
+		self.threads.append(roundThread)
+		#try:
+		while self.alive.isSet():
+			connection, client_address = self.sock.accept()
+			new_player = Player(connection, client_address)
+			self.players.append(new_player)
+			playerThread = threading.Thread(target=self.doPlayer ,args=(new_player,))
+			playerThread.start()
+			self.threads.append(playerThread)
+				
+		#except Exception as e:
+		#	self.log('error: %s' % e)
+		self.cleanup()	
+	
+	def report(self, msg):
+		# This function is too complicated to properly comment
+		sys.stdout.flush()
+
+		now = str(datetime.datetime.now())
+		
+		# Grab the lock
+		log_file = self._settings['log_file']
+		try:
+			with FileLock(log_file), open(log_file, 'a') as f:
+				f.write('%s:\t' % self.now())
+				f.write(msg + '\n')
+		except OSError:
+			pass
+
+		return msg	
+	
+	def log(self, msg, log_level=0):
+		if log_level <= self._settings['verbose']:
+			print msg
+			return self.report(msg)
+		else:
+			return msg # This is pythonic	
+	
+	def cleanup(self):
+		# Clean up threads
+		self.log("Attempting to close threads...", 10)
+		self.alive.clear() # Unset alive, this informs the class that no more server actions should take place
+		for thread in self.threads:
+			thread.join()
+		threads = []
+		self.log("Threads successfully closed", 10)
+
+		# Clean up sockets
+		self.log("Terminating active connections...", 10)
+		for player in self.players:
+			try:
+				player.connection.shutdown(SHUT_RDWR)
+			except:
+				pass
+			try:
+				player.connection.close()
+			except:
+				pass			
+		self.log("Active connections terminated", 10)	
+		
+		
+if __name__ == "__main__":
+	import sys
+	server = GameServer(sys.argv[1])
+	server.run()
